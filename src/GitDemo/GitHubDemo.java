1ab87a6330bfcea530634eff6bcc825c37a621b6
johnwanggithub <john83.wang@gmail.com>
2014-10-19 17:53:26 +0800

    update GitHubDemo.java & add DemoMain.java
    Signed-off-by: johnwanggithub <john83.wang@gmail.com>

--- src/GitDemo/GitHubDemo.java
+++ src/GitDemo/GitHubDemo.java
@@ -1,6 +1,18 @@
 package GitDemo;
 
-public class GitHubDemo {
-	private String msg="Git Hub Demo";
+//import java.io.*;
+//import java.lang.*;
 
+public class GitHubDemo {
+	public String msg;
+	
+	protected void getMsg(){
+		System.out.print(msg);
+	}
+	
+	protected void setMsg(){
+		msg = "Demo for GitHub";
+	}
 }
+
+
